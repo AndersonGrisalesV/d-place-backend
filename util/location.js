7956e67d7d39b906ab42c946f69fc0e57310af2d
AndersonGrisalesV <grisalesvargasanderson@gmail.com>
2022-11-29 19:01:26 -0500

    November 29, 2022

--- util/location.js
+++ util/location.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const HttpError = require("../models/http-error");
 
-const API_KEY = "";
+const API_KEY = " ";
 
 async function getCoordinatesForAddress(address) {
   const response = await axios.get(
